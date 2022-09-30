30e8f9a6927d1979b5a222c147ff9d85bdad3234
Maya Mori <90734378+ka2-penguin@users.noreply.github.com>
2022-09-30 15:56:01 -0400

    updated header and wrote better comments

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,10 +1,9 @@
 """
 Humans: Maya Mori, Justin Mohabir
-Duckies: Catbat, Alfred
 Softdev
-06_py-csv
+k06 -- read csv files and use random different weights
 2022-09-29
-time spent: 1.0 hr
+time spent: 1.2 hr
 DISCO:
   - TypeError: 'dict_keys' object is not subscriptable
   - csv.reader(some_csv_file) will separate the values and deals with extra
@@ -13,7 +12,9 @@ DISCO:
 QCC:
 
 HOW THIS SCRIPT WORKS:
-  - We read the file using csv.reader() into data with each row in a list
+  - We read the file and split it by newlines and quotes.
+  - If that line had double quotes, look at the last 2 terms. Else, split
+  the line by commas.
   - We created a dictionary with occupation as the key and the percentage as
   the value
   - Then, we used random.choices() in random_occupation to get a weighted
@@ -22,7 +23,6 @@ HOW THIS SCRIPT WORKS:
 """
 
 import random as rg
-
 import csv
 
 data1 = []
@@ -30,25 +30,24 @@ data = []
 
 with open('occupations.csv','r') as csvfile:
     #data = list(csv.reader(csvfile))
-    data0 = csvfile.read().split('\n')
+    data0 = csvfile.read().split('\n')      # split each line
     for row in data0:
-        data1 += [row.split("\"")]
-        
+        data1 += [row.split("\"")]          # remove quotes
+
 for row in data1:
     if len(row) == 1:
         data += [row[0].split(',')]
     else:
         data += [[row[1],row[-1][1:]]]
 
-print(data)
-
-
+#print(data)
 
 data = data[1:-2]
 
 dict_data = {}
-tester = {}
+tester = {}                                 # Creates a dict for testing
 
+# makes the dict with occupation names as keys and percents as values
 for row in data:
     dict_data[row[0]] = float(row[1])
     tester[row[0]] = 0
@@ -56,7 +55,7 @@ for row in data:
 print(dict_data)
 
 def random_occupation():
-    return rg.choices(list(dict_data.keys()),\
+    return rg.choices(list(dict_data.keys()),\      # Returns a list len 1
            weights=list(dict_data.values()))[0]
 
 #tester = {}
@@ -66,7 +65,7 @@ for i in range(1000000):
     if occ not in tester:
         tester[occ] = 0
     tester[occ] += 1
-    
+
 print(tester)
 
 '''
@@ -75,7 +74,7 @@ w = [1.0,2.0,3.5]
 
 for i in range(10):
     print(rg.choices(l,weights=w))
-    
+
 data = {1:0,2:0,3:0}
 
 for i in range(100000):
@@ -83,4 +82,3 @@ for i in range(100000):
 
 print(data)
 '''
-
