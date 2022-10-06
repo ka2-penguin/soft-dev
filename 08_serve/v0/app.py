05c5a0b893598589635db9b72ff69e35c87541f0
Maya Mori <90734378+ka2-penguin@users.noreply.github.com>
2022-10-06 13:53:51 -0400

    done with v0

--- 08_serve/v0/app.py
+++ 08_serve/v0/app.py
@@ -1,14 +1,29 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2022
+'''
+<Frist> <Lsat>
+SoftDev
+K<nn> -- <Title/Topic/Summary... (Aim for concision, brevity, CLARITY. Write to your future self...)>
+<yyyy>-<mm>-<dd>
+time spent: <elapsed time in hours, rounded to nearest tenth>
+'''
 
 from flask import Flask
-app = Flask(__name__) # ...
 
-@app.route("/") # ...
+# why are there underscores before and after 'name'?
+app = Flask(__name__) # assigns new instance of Flask to app
+#print(__name__)
+
+@app.route("/") # root route request, slash is needed
+
+# function runs regardless of its name
 def hello_world():
-    print(__name__) # ...
-    return "No hablo queso!"  # ...
+    print('hello')#__name__) # why does this print '__main__'?
+    return 'hi'#"No hablo queso!"  # what is written in web browser
 
-app.run()  # ...
+app.run()  # Flask app runs
+
+'''
+- whatever is returned in hello_world is displayed on web browser
+- whatever is printed in the function prints in the terminal
+- the string in @app.route() needs to follow the localhost url
+'''
                 
