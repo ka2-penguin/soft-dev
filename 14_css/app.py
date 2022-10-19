e173b15b24dc46e38e3bb206e2908ee6e6dfffa2
Maya Mori <90734378+ka2-penguin@users.noreply.github.com>
2022-10-19 13:25:17 -0400

    added headings and notes

--- 14_css/app.py
+++ 14_css/app.py
@@ -1,3 +1,11 @@
+'''
+Metamorphosis: Eric Sohel, Yuki Feng, Maya Mori, Aleksandra Shifrina
+SoftDev
+k14 -- CSS in Flask
+2022-10-19
+time spent: 0.3 
+'''
+
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
@@ -38,27 +46,6 @@ def disp_loginpage():
     # print("***DIAG: request.headers ***")
     # print(request.headers)
     return render_template( 'index.html' )
-
-
-# @app.route("/auth", methods=['GET', 'POST'])
-# def authenticate():
-#     print("\n\n\n")
-#     print("***DIAG: this Flask obj ***")
-#     print(app)
-#     print("***DIAG: request obj ***")
-#     print(request)
-#     print("***DIAG: request.args ***")
-#     print(request.args)
-#     print("***DIAG: request.form['username']  ***")
-#     print(request.form['username'])
-#     print("***DIAG: request.headers ***")
-#     print(request.headers)
-#     return render_template('response.html',\
-#     username=request.form['username'],\
-#     request_type=request.method)
-#     return "Waaaa hooo HAAAH"  #response to a form submission
-
-
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
