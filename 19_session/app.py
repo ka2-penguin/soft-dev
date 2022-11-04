24e34ebb9439208692a70102ad0f489a524e30a7
Maya Mori <90734378+ka2-penguin@users.noreply.github.com>
2022-11-04 14:05:19 -0400

    classwork

--- 19_session/app.py
+++ 19_session/app.py
@@ -10,8 +10,41 @@ from flask import Flask             #facilitate flask webserving
 from flask import render_template 
 from flask import request
 
+from flask import session
+
 app = Flask(__name__)
 
-@app.route("/")
-def disp_loginpage():
-    request.cookies.get()
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'foo'
+username = 'bob'
+password = 'mob123'
+
+
+
+
+@app.route('/')
+def index():
+    print(session)
+     session.pop('username', None)
+    if 'username' in session:
+        return render_template('response.html',username=session["username"])
+    return render_template('login.html',good_password=password)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        if request.form['username'] == username and request.form['password'] == password:
+            return render_template('response.html')
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
