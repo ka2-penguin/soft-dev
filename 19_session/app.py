70ec272a0934970a0679c614a59965aa4dfb84d2
Maya Mori <90734378+ka2-penguin@users.noreply.github.com>
2022-11-05 13:38:40 -0400

    error messages for wrong username and password

--- 19_session/app.py
+++ 19_session/app.py
@@ -3,7 +3,7 @@ Metamorphosis: Eric Sohel, Yuki Feng, Maya Mori, Aleksandra Shifrina
 Softdev
 k12 -- GET vs. POST, displaying form responses
 2022-10-17
-time spent: 0.8
+time spent: 1.2
 '''
 
 from flask import Flask             #facilitate flask webserving
@@ -19,29 +19,42 @@ app.secret_key = b'foo'
 username = 'bob'
 password = 'mob123'
 
-
-
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     print(session)
-     session.pop('username', None)
+    # print(request.cookies)
+    print(request.cookies.get('username'))
+    # if request.cookies.get('username') is not None:
     if 'username' in session:
         return render_template('response.html',username=session["username"])
-    return render_template('login.html',good_password=password)
+    return render_template('login.html',has_error=False)
+    # session.pop('username', None)
+    # if 'username' in session:
+    #     return render_template('response.html',username=session["username"])
+    # return render_template('login.html',good_password=password)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def login():
-    if request.method == 'POST':
+    print('login function')
+    if request.form['username'] == username and request.form['password'] == password:
         session['username'] = request.form['username']
-        if request.form['username'] == username and request.form['password'] == password:
-            return render_template('response.html')
+        return render_template('response.html',username=session["username"])
+    # if request.method == 'POST':
 
-@app.route('/logout')
+    error_type = ''
+    if request.form['username'] != username:
+        error_type='username'
+    elif request.form['password'] != password:
+        error_type = 'password'
+
+    return render_template('login.html',has_error=True,error=error_type)
+    # session['username'] = request.form['username']
+
+@app.route('/logout',methods=['POST'])
 def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
-    return redirect(url_for('index'))
+    return render_template('login.html')
 
 
 if __name__ == "__main__": #false if this file imported as module
