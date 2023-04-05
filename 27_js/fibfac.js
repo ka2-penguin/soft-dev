9272355dd655df0f155088057bf7170e35bbce6d
Maya Mori <90734378+ka2-penguin@users.noreply.github.com>
2023-04-04 21:19:25 -0400

    homework

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,7 +1,7 @@
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// Team Wandering Majestic Merciful Mice:: Maya Mori, Wilson Mach
 // SoftDev pd2
 // K27 -- Basic functions in JavaScript
-// 2023-04-04t
+// 2023-04-04
 // --------------------------------------------------
 
 
@@ -9,5 +9,34 @@
 // pair programming style,
 // implement a fact and fib fxn
 
+// (define (fact n) 
+//   (if (= n 0) 
+//       1 
+//       (* n (fact (- n 1)))
+//   )
+// )
+
+function fact(n){
+    if(n == 0){
+        return 1;
+    } 
+
+    return n * fact(n-1);
+}
+
+function fib(n) {
+    if(n == 0){
+        return 0;
+    }
+
+    if (n == 1){
+        return 1;
+    }
+
+    return fib(n-1) + fib(n-2);    
+}
+
+// let x = fib(3);
+// console.log(x);
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
